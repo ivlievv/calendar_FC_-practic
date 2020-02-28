033fc7c8a65e0ab6afb88ef010c62b13a63c48cd
ivlievv <igor852745@gmail.com>
2020-02-28 16:28:00 +0200

    create sass, and bad chances css in sass

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import './App.css';
-import EventItem from './components/Date';
-import { mdiChevronDown } from '@mdi/js';
+import './App.scss';
 
-function App() {
-return(
-  <div className="container">
-    <div className="header">
-      <h1 className="calendar">Calendar</h1>
-    </div>
-        <div className="calendarNav">
-          <h3 className="dateNav">May</h3>
-          <h3 className="btnMonth">June</h3>
-          <h3 className="dateNav">Jul</h3>
-        </div>
+
+function App () {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 className="calendar">Calendar</h1>
+      </div>
+      <div className="calendarNav">
+        <h3 className="dateNav">May</h3>
+        <h3 className="btnMonth">June</h3>
+        <h3 className="dateNav">Jul</h3>
+      </div>
       <div className="weekContainer">
         <h3>S</h3>
         <h3>M</h3>
@@ -25,40 +24,56 @@ return(
       </div>
       <div className="calendarContainer">
 
-        <h3 className="data">1</h3>
-        <h3 className="data">2</h3>
-        <h3 className="data">3</h3>
-        <h3 className="data">4</h3>
-        <h3 className="data">5</h3>
-        <h3 className="data">6</h3>
-        <h3 className="data">7</h3>
-        <h3 className="data">8</h3>
-        <h3 className="data">9</h3>
-        <h3 className="data">10</h3>
-        <h3 className="data">11</h3>
-        <h3 className="data">12</h3>
-        <h3 className="data">13</h3>
-        <h3 className="data">14</h3>
-        <h3 className="data">15</h3>
-        <h3 className="data">16</h3>
-        <h3 className="data">17</h3>
-        <h3 className="data">18</h3>
-        <h3 className="data">20</h3>
-        <h3 className="data">21</h3>
-        <h3 className="data">22</h3>
-        <h3 className="data">23</h3>
-        <h3 className="data">24</h3>
-        <h3 className="data">25</h3>
-        <h3 className="data">26</h3>
-        <h3 className="data">27</h3>
-        <h3 className="data">28</h3>
-        <h3 className="data">29</h3>
-        <h3 className="data">30</h3>
-        <h3 className="data">31</h3>
+        <div className="oneWeek">
+          <span className="data">1</span>
+          <span className="data">2</span>
+          <span className="data">3</span>
+          <span className="data">4</span>
+          <span className="data">5</span>
+          <span className="data">6</span>
+          <span className="data">7</span>
+        </div>
+        <div className="twoWeek">
+          <span className="data">8</span>
+          <span className="data">9</span>
+          <span className="data">10</span>
+          <span className="data">11</span>
+          <span className="data">12</span>
+          <span className="data">13</span>
+          <span className="data">14</span>
+        </div>
+        <div className="threeWeek">
+          <span className="data">15</span>
+          <span className="data">16</span>
+          <span className="data">17</span>
+          <span className="data">18</span>
+          <span className="data">18</span>
+          <span className="data">19</span>
+          <span className="data">20</span>
+        </div>
+        <div className="fourWeek">
+          <span className="data">21</span>
+          <span className="data">22</span>
+          <span className="data">23</span>
+          <span className="data">24</span>
+          <span className="data">25</span>
+          <span className="data">26</span>
+          <span className="data">27</span>
+        </div>
+        <div className="fiveWeek">
+          <span className="data">28</span>
+          <span className="data">29</span>
+          <span className="data">30</span>
+          <span className="data">31</span>
+          <span className="data">1</span>
+          <span className="data">2</span>
+          <span className="data">3</span>
+        </div>
+
       </div>
 
-  </div>
-)
+    </div>
+  );
 }
 
 export default App;
